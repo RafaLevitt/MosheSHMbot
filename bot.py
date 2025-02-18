96f71155bd575988852fb8b3beab410431169ca1
RafaLevitt <rafkin.a@gmail.com>
2025-02-18 18:06:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import ParseMode
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 from aiogram.utils import executor
 
